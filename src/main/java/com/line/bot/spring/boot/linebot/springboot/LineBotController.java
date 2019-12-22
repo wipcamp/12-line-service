2e6d4eab89177f8675a4727b42303e5427a57b05
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-23 00:36:49 +0700

    [Add]LineBotController.java, [Update]Application.java change echo function to LineBotController.java

---
+++ src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
@@ -0,0 +1,21 @@
+package com.line.bot.spring.boot.linebot.springboot;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+
+@LineMessageHandler
+public class LineBotController {
+
+    @EventMapping
+    public Message handleTextMessage(MessageEvent<TextMessageContent> e){
+        System.out.println("Event: " + e);
+        TextMessageContent message = e.getMessage();
+        return new TextMessage(message.getText());
+    }
+
+}
