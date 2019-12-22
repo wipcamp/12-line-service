315bdf2005cc6c08b4ae49476fe2b2b80aa72308
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-22 23:40:19 +0700

    [Update]Application.java

--- src/main/java/com/line/bot/spring/boot/linebot/springboot/Application.java
+++ src/main/java/com/line/bot/spring/boot/linebot/springboot/Application.java
@@ -2,13 +2,26 @@ package com.line.bot.spring.boot.linebot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
+@LineMessageHandler
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
 
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e){
+		System.out.println("Event: " + e);
+		TextMessageContent message = e.getMessage();
+		return new TextMessage(message.getText());
 	}
 
 }
