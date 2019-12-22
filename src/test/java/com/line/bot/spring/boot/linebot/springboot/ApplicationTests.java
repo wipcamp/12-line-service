c8fba269d5ac275ad181ff359ae8b1a70ed3a358
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-22 20:23:55 +0700

    Init commit

---
+++ src/test/java/com/line/bot/spring/boot/linebot/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.line.bot.spring.boot.linebot.springboot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
