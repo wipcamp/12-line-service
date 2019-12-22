d2862a4ef7aa51218f2c33312ed88440c287a593
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-23 01:08:05 +0700

    [Update]LineBotController.java for return user data

--- src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
+++ src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
@@ -1,21 +1,99 @@
 package com.line.bot.spring.boot.linebot.springboot;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.xml.soap.Text;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
 @LineMessageHandler
 public class LineBotController {
 
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
     @EventMapping
-    public Message handleTextMessage(MessageEvent<TextMessageContent> e){
-        System.out.println("Event: " + e);
-        TextMessageContent message = e.getMessage();
-        return new TextMessage(message.getText());
+    public void handleTextMessage(MessageEvent<TextMessageContent> event){
+        log.info(event.toString());
+        TextMessageContent message = event.getMessage();
+        handleTextContent(event.getReplyToken(), event, message);
     }
+
+    private void handleTextContent(String replyToken, Event event, TextMessageContent content){
+
+        String text = content.getText();
+
+        log.info("Got text message from %s : %s", replyToken, text);
+
+        switch (text){
+            case "Profile" : {
+                String userId = event.getSource().getUserId();
+                if(userId != null){
+                    lineMessagingClient.getProfile(userId)
+                            .whenComplete((profile, throwable) -> {
+                                if(throwable != null){
+                                    this.replyText(replyToken, throwable.getMessage());
+                                    return;
+                                }
+                                this.reply(replyToken, Arrays.asList(
+                                        new TextMessage("Display name: " + profile.getDisplayName()),
+                                        new TextMessage("Status message: " + profile.getStatusMessage()),
+                                        new TextMessage("User ID: " + profile.getUserId())
+                                ));
+                            });
+                }
+                break;
+            }
+            default:
+                log.into("Return echo message %s : %s", replyToken, text);
+                this.replyText(replyToken);
+        }
+
+    }
+
+    private void replyText(@NonNull String replyToken, @NonNull String message){
+        if(replyToken.isEmpty()){
+            throw new IllegalArgumentException("reply Token is not empty");
+        }
+
+        if(message.length() > 1000){
+            message = message.substring(0, 1000 - 2) + "...";
+        }
+        this.reply(replyToken, new TextMessage(message));
+
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, Collections.singletonList(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        try {
+            BotApiResponse response = lineMessagingClient.replyMessage(
+                    new ReplyMessage(replyToken, messages)
+            ).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 
 }
