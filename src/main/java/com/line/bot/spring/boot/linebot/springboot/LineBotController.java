091e8e5996a631512ba7cfc6480c583e91dec121
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-23 20:12:06 +0700

    [Update] LineBotController.java check reply obj. profile

--- src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
+++ src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
@@ -56,11 +56,33 @@ public class LineBotController {
                                                 profile.getStatusMessage()),
                                         new TextMessage("User ID: " +
                                                 profile.getUserId())
+
                                 ));
                             });
                 }
                 break;
             }
+            case "Check": {
+                String userId = event.getSource().getUserId();
+                if(userId != null) {
+                    lineMessagingClient.getProfile(userId)
+                            .whenComplete((profile, throwable) -> {
+                                if(throwable != null) {
+                                    this.replyText(replyToken, throwable.getMessage());
+                                    return;
+                                }
+                                this.reply(replyToken, Arrays.asList(
+                                        new TextMessage("User ID: " +
+                                                profile.getUserId()),
+                                        new TextMessage("Profile: " +
+                                                profile.toString())
+
+                                ));
+                            });
+                }
+                break;
+            }
+
             default:
                 log.info("Return echo message %s : %s", replyToken, text);
                 this.replyText(replyToken, text);
