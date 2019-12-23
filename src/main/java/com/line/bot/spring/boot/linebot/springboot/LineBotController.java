0ac4326741547378ab0488a2be2e281b6deae102
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-23 11:49:14 +0700

    [Update] LineBotController.java for get user data

--- src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
+++ src/main/java/com/line/bot/spring/boot/linebot/springboot/LineBotController.java
@@ -15,68 +15,67 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.xml.soap.Text;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @LineMessageHandler
 public class LineBotController {
-
     @Autowired
     private LineMessagingClient lineMessagingClient;
 
     @EventMapping
-    public void handleTextMessage(MessageEvent<TextMessageContent> event){
+    public void handleTextMessage(MessageEvent<TextMessageContent> event) {
         log.info(event.toString());
         TextMessageContent message = event.getMessage();
         handleTextContent(event.getReplyToken(), event, message);
     }
 
-    private void handleTextContent(String replyToken, Event event, TextMessageContent content){
-
+    private void handleTextContent(String replyToken, Event event,
+                                   TextMessageContent content) {
         String text = content.getText();
 
         log.info("Got text message from %s : %s", replyToken, text);
 
-        switch (text){
-            case "Profile" : {
+        switch (text) {
+            case "Profile": {
                 String userId = event.getSource().getUserId();
-                if(userId != null){
+                if(userId != null) {
                     lineMessagingClient.getProfile(userId)
                             .whenComplete((profile, throwable) -> {
-                                if(throwable != null){
+                                if(throwable != null) {
                                     this.replyText(replyToken, throwable.getMessage());
                                     return;
                                 }
                                 this.reply(replyToken, Arrays.asList(
-                                        new TextMessage("Display name: " + profile.getDisplayName()),
-                                        new TextMessage("Status message: " + profile.getStatusMessage()),
-                                        new TextMessage("User ID: " + profile.getUserId())
+                                        new TextMessage("Display name: " +
+                                                profile.getDisplayName()),
+                                        new TextMessage("Status message: " +
+                                                profile.getStatusMessage()),
+                                        new TextMessage("User ID: " +
+                                                profile.getUserId())
                                 ));
                             });
                 }
                 break;
             }
             default:
-                log.into("Return echo message %s : %s", replyToken, text);
-                this.replyText(replyToken);
+                log.info("Return echo message %s : %s", replyToken, text);
+                this.replyText(replyToken, text);
         }
-
     }
 
-    private void replyText(@NonNull String replyToken, @NonNull String message){
-        if(replyToken.isEmpty()){
-            throw new IllegalArgumentException("reply Token is not empty");
+    private void replyText(@NonNull  String replyToken, @NonNull String message) {
+        if(replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken is not empty");
         }
 
-        if(message.length() > 1000){
+        if(message.length() > 1000) {
             message = message.substring(0, 1000 - 2) + "...";
         }
         this.reply(replyToken, new TextMessage(message));
-
     }
 
     private void reply(@NonNull String replyToken, @NonNull Message message) {
@@ -92,8 +91,5 @@ public class LineBotController {
             throw new RuntimeException(e);
         }
     }
-
-
-
 
 }
