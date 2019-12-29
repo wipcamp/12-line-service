848bf327c20894cceb6dba8af2a82edd9d09b8b0
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-12-30 00:46:05 +0700

    [Add] Verify in response pack

--- src/main/java/com/line/service/spring/boot/linelogin/api/response/Verify.java
+++ src/main/java/com/line/service/spring/boot/linelogin/api/response/Verify.java
@@ -1,4 +1,15 @@
 package com.line.service.spring.boot.linelogin.api.response;
 
 public class Verify {
+
+    public final String scope;
+    public final String client_id;
+    public final Integer expires_in;
+
+
+    public Verify(String scope, String client_id, Integer expires_in) {
+        this.scope = scope;
+        this.client_id = client_id;
+        this.expires_in = expires_in;
+    }
 }
