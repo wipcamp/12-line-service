7967160ec8da1dfe4f24834fa7237175b932bc5b
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-01-03 20:03:46 +0700

    [Add] /recieveToken

--- src/main/java/linebot/linelogin/controller/ApiController.java
+++ src/main/java/linebot/linelogin/controller/ApiController.java
@@ -1,4 +1,21 @@
 package linebot.linelogin.controller;
 
+import linebot.linelogin.service.LineAPIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class ApiController {
+
+    @Autowired
+    private LineAPIService lineAPIService;
+
+    @GetMapping("/recieveToken")
+    public @ResponseBody ResponseEntity<String> recieveToken(){
+        return new ResponseEntity<String>("Get Response", HttpStatus.OK);
+    }
+
+
 }
