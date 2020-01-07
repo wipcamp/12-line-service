ad84ed0172301face4b207536ef892104a7d4083
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-01-07 18:27:23 +0700

    [Update] rest controller add api for auth game

--- src/main/java/linebot/linelogin/controller/ApiController.java
+++ src/main/java/linebot/linelogin/controller/ApiController.java
@@ -5,10 +5,10 @@ import linebot.linelogin.service.LineAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ApiController {
@@ -21,12 +21,17 @@ public class ApiController {
         return new ResponseEntity<String>("Get Response", HttpStatus.OK);
     }
 
-    @GetMapping("/authForGame")
-    public @ResponseBody ResponseEntity<String> authForGame(@RequestParam(value = "code", required = false) String code,
-                                   @RequestParam(value = "state", required = false) String state,
-                                   @RequestParam(value = "scope", required = false) String scope) {
+    @PostMapping("/authForGame")
+    public @ResponseBody ResponseEntity<AccessToken> authForGame(
+            @RequestParam(value = "code", required = false) String code,
+            HttpServletResponse response) {
         AccessToken token = lineAPIService.gameAccessToken(code);
-        return new ResponseEntity<String>(token.id_token, HttpStatus.OK);
+        Cookie cookie = new Cookie("token", token.access_token);
+        response.addCookie(cookie);
+        return new ResponseEntity<AccessToken> (token, HttpStatus.OK);
     }
+
+
+
 
 }
