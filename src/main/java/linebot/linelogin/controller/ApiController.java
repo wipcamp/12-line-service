99f262e14d4ac933dc7ed5844e939f7082541ce6
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-01-15 18:03:40 +0700

    [Update] add cookie from httpservletresponse

--- src/main/java/linebot/linelogin/controller/ApiController.java
+++ src/main/java/linebot/linelogin/controller/ApiController.java
@@ -31,12 +31,12 @@ public class ApiController {
     @GetMapping("/auth")
     public @ResponseBody ResponseEntity<LineResponse> authForGame(
             @RequestParam(value = "code", required = false) String code,
-            @RequestParam(value = "nonce", required = false) String nonce) {
+            @RequestParam(value = "nonce", required = false) String nonce,
+            HttpServletResponse response) {
         AccessToken access_token = lineAPIService.gameAccessToken(code);
         IdToken id_token = lineAPIService.idToken(access_token.id_token);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Set-Cookie","platform=mobile; Max-Age=604800; Path=/; Secure; HttpOnly");
-        ResponseEntity.status(HttpStatus.OK).headers(headers).build();
+        Cookie c = new Cookie("name", "test");
+        response.addCookie(c);
         if(nonce.equals(id_token.nonce)){
             LineResponse lineRes = new LineResponse(access_token.scope, access_token.access_token, access_token.token_type, access_token.expires_in,
                     access_token.id_token,id_token.sub);
