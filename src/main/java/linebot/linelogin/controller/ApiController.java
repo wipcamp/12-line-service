3886701b15e3d1435febf26372bda18bdb957568
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-01-18 16:10:12 +0700

    [Update] encode and decode JWt

--- src/main/java/linebot/linelogin/controller/ApiController.java
+++ src/main/java/linebot/linelogin/controller/ApiController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import retrofit2.http.GET;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -31,12 +32,12 @@ public class ApiController {
     @GetMapping("/auth")
     public @ResponseBody ResponseEntity<LineResponse> authForGame(
             @RequestParam(value = "code", required = false) String code,
-            @RequestParam(value = "nonce", required = false) String nonce,
-            HttpServletResponse response) {
+            @RequestParam(value = "nonce", required = false) String nonce) {
         AccessToken access_token = lineAPIService.gameAccessToken(code);
         IdToken id_token = lineAPIService.idToken(access_token.id_token);
-        Cookie c = new Cookie("name", "test");
-        response.addCookie(c);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Set-Cookie","platform=mobile; Max-Age=604800; Path=/; HttpOnly");
+        ResponseEntity.status(HttpStatus.OK).headers(headers).build();
         if(nonce.equals(id_token.nonce)){
             LineResponse lineRes = new LineResponse(access_token.scope, access_token.access_token, access_token.token_type, access_token.expires_in,
                     access_token.id_token,id_token.sub);
@@ -50,6 +51,27 @@ public class ApiController {
         String sth = lineAPIService.getGenerateCode();
         System.out.println(sth);
         return new ResponseEntity<String>(sth, HttpStatus.OK);
+    }
+
+    @GetMapping("/createToken")
+    public @ResponseBody ResponseEntity<String> createToken(
+            @RequestParam(value = "scope", required =false) String scope,
+            @RequestParam(value = "access_token", required =false) String access_token,
+            @RequestParam(value = "token_type", required =false) String token_type,
+            @RequestParam(value = "expires_in", required = false) int expires_in,
+            @RequestParam(value = "id_token", required =false) String id_token,
+            @RequestParam(value = "userId") String userId
+    ){
+        LineResponse line = new LineResponse(scope,access_token,token_type,expires_in,id_token,userId);
+        String token = lineAPIService.createToken(line);
+        return new ResponseEntity<String>((String) token, HttpStatus.OK);
+    }
+
+    @GetMapping("/getToken")
+    public @ResponseBody ResponseEntity<LineResponse> getToken(
+            @RequestParam(value = "token") String token){
+        LineResponse line = lineAPIService.decodeToken(token);
+        return new ResponseEntity<LineResponse>((LineResponse) line, HttpStatus.OK);
     }
 
 }
