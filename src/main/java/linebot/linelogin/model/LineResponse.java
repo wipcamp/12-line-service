868be93a198e31dbb040238c97451bcc1f31726a
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-02 22:54:26 +0700

    [Update] add name in line response json

--- src/main/java/linebot/linelogin/model/LineResponse.java
+++ src/main/java/linebot/linelogin/model/LineResponse.java
@@ -8,13 +8,15 @@ public class LineResponse {
     public final int expires_in;
     public final String id_token;
     public final String userId;
+    public final String name;
 
-    public LineResponse(String scope, String access_token, String token_type, int expires_in, String id_token, String userId) {
+    public LineResponse(String scope, String access_token, String token_type, int expires_in, String id_token, String userId, String name) {
         this.scope = scope;
         this.access_token = access_token;
         this.token_type = token_type;
         this.expires_in = expires_in;
         this.id_token = id_token;
         this.userId = userId;
+        this.name = name;
     }
 }
